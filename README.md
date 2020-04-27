8da506ad638e7b7843df445136f83960dd836b3b
Gayathiri Ramamoorthy <rgayathiri2001@yahoo.com>
2020-04-26 20:32:49 -0400

    updated readme and comment

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# Note_taker
+ # Project Title:
+
+  ![Badge](https://img.shields.io/static/v1?label=Note_Taker&message=projecttitle&color=blueviolet)
+  
+  ## *Note Taker*
+
+  # Project description:
+  ### This section describe the project detail.
+
+  ### This note taker application use to create a notes with detail, once the details are entered a save button appeared to save the notes. User also can delete the notes using the delete button. Application creates a localhost web server dynamically and perform the http operations such as GET, POST and DELETE operations based on the user activity in the application.
+  ### This application deployed in Heroku
+  ### This application uses HTML,Javascript,Node.js and Heroku.
--- server.js
+++ server.js
@@ -1,69 +1,69 @@
+// Dependency modules
 const fs = require('fs');
-const express = require ('express');
-const path = require ('path');
+const express = require('express');
+const path = require('path');
+const notes = require('./db/db.json');
 
+//Sets up the Express App and port number
 const app = express();
 const PORT = process.env.PORT || 3000
 
+//Output path to save the file
 const OUTPUT_DIR = path.resolve(__dirname, "db")
 const outputPath = path.join(OUTPUT_DIR, "db.json");
 
+// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname + '/public'));
-const notes = require ('./db/db.json');
+
 
 let id_num = 1
 
+//get html route request
 app.get('/notes', function (req, res) {
     res.sendFile(path.join(__dirname, 'public/notes.html'));
-  });
-  app.get('/api/notes', function( req,res){
+});
+
+//get api route request
+app.get('/api/notes', function (req, res) {
     console.log(notes)
     res.json(notes)
-
 })
 
-  app.get ('*',function(req, res){
-      res.sendFile(path.join(__dirname,'public/index.html'))
-  })
+// If no matching route is found default to home
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+})
 
-  app.post('/api/notes', function (req, res) {
+//api to post resquest and create newNote with id 
+app.post('/api/notes', function (req, res) {
     const newNote = req.body
-   newNote.routeName = newNote.title.replace(/\s+/g, "").toLowerCase();
-   newNote.id = id_num++
+    newNote.routeName = newNote.title.replace(/\s+/g, "").toLowerCase();
+    newNote.id = id_num++
     notes.push(newNote)
-   fs.writeFile(outputPath, JSON.stringify(notes), function (err) {
-    //fs.writeFile('./db/db.json', JSON.stringify(notes), function (err) {
-      if (err) {
-          throw err
-      }
-  })
+    fs.writeFile(outputPath, JSON.stringify(notes), function (err) {
+        if (err) {
+            throw err
+        }
+    })
     res.json(newNote)
-  })
+})
 
-  app.delete('/api/notes/:id', function (req, res) {
-   let noteId = parseInt(req.params.id);
+//api to delete based on provided id
+app.delete('/api/notes/:id', function (req, res) {
+    let noteId = parseInt(req.params.id);
     for (let i = 0; i < notes.length; i++) {
         if (noteId === notes[i].id) {
             notes.splice(i, 1);
-           
             fs.writeFile(outputPath, JSON.stringify(notes), function (err) {
                 if (err) {
                     throw err
-                } 
+                }
             })
         }
     }
     res.json(notes)
 })
-
-
-
-
-
-
-
-
 
 app.listen(PORT, () => console.log(`Server listening on PORT: ${PORT}`))
